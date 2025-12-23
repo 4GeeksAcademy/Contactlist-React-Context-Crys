aa5260e00e6e1491a24d2a168cf45e7df24eb752
Crys Carmona <crysc4rmon4@gmail.com>
2025-12-23 14:27:37 +0000

    logotipo y ico ok, pendiente css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
-import  useGlobalReducer  from "../hooks/useGlobalReducer"; // si luego manejamos modal o acciones
+import useGlobalReducer from "../hooks/useGlobalReducer"; // si luego manejamos modal o acciones
+import logo from "../assets/logotipo.jpg";
 
 export const Navbar = () => {
     const { dispatch } = useGlobalReducer(); // opcional si quieres usarlo para abrir modal
@@ -9,11 +10,17 @@ export const Navbar = () => {
             <div className="container d-flex justify-content-between align-items-center">
 
                 {/* Título con logo */}
-                <Link to="/" className="navbar-title d-flex align-items-center text-decoration-none">
-                    <i className="bi bi-people-fill me-2"></i> {/* Icono Bootstrap */}
+                <Link
+                    to="/"
+                    className="navbar-title d-flex align-items-center text-decoration-none"
+                >
+                    <img
+                        src={logo}
+                        alt="ContactMaster logo"
+                        className="me-2 navbar-logo"
+                    />
                     <span className="h4 mb-0">ContactMaster</span>
                 </Link>
-
                 {/* Botón agregar contacto */}
                 <div className="navbar-actions">
                     <Link to="/contacts/add">  {/* más adelante será ruta de formulario */}
